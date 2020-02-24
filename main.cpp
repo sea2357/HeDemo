9f6fccb72ba1a93a176db9ced534536ed098cd96
sea2357 <sea2357@163.com>
2020-02-24 17:30:43 +0800

    Dev: add plaintext picture recognition

--- main.cpp
+++ main.cpp
@@ -16,27 +16,31 @@ int main()
     uint32_t num;
     while (true)
     {
+        KNN knn;
+        string pic;
+        string data_path = "../data";
+        size_t num = 70;
         cout << "please select:" << endl;
-        cout << "1. test" << endl;
+        cout << "1. test using default parameters" << endl;
         cout << "2. recognize a picture" << endl;
         cout << "3. recognize an encrypted picture" << endl;
-        cout << "4. exit" << endl;
+        cout << "4. test the success rate" << endl;
+        cout << "5. exit" << endl;
         cin >> num;
         switch (num)
         {
         case 1:
         {
-            KNN knn;
-            string data_path = "../data";
-            knn.test(data_path);
+            pic = "../data/4.bmp";
+            cout << "recognize 4.bmp using plaintext......" << endl;
+            knn.recognize(data_path, pic, num);
+            cout << "recognize 4.bmp using ciphertext......" << endl;
+            knn.ciphertext_recognize(data_path, pic, num);
             return 0;
         }
         case 2:
         {
-            KNN knn;
-            std::string pic;
-            string data_path = "../data";
-            cout << "please specify a image file " << endl;
+            cout << "please specify a image file, e.g. ../data/4.bmp " << endl;
             cin >> pic;
             size_t num = 70;
             knn.recognize(data_path, pic, num);
@@ -44,10 +48,7 @@ int main()
         }
         case 3:
         {
-            KNN knn;
-            std::string pic;
-            string data_path = "../data";
-            cout << "please specify a image file " << endl;
+            cout << "please specify a image file, e.g. ../data/4.bmp " << endl;
             cin >> pic;
             size_t num = 70;
             knn.ciphertext_recognize(data_path, pic, num);
@@ -55,11 +56,15 @@ int main()
         }
         case 4:
         {
+            knn.test(data_path);
+        }
+        case 5:
+        {
             return 0;
         }
         default:
         {
-            cout << "please select 1, 2 or 3. Try again!" << endl;
+            cout << "please select 1, 2, 3, 4 or 5. Try again!" << endl;
         }
         }
     }
